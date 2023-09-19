386e040efcdd1a9f6443a2cdc43d2717f9c013e6
“yuantao2333@qq.com” <2685275149@qq.com>
2023-09-19 15:22:27 +0800

    加了一个index.html

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,3 @@
  # .flaskenv 文件
 FLASK_DEBUG=1
-FLASK_APP = "hello.py"
+FLASK_APP = "app.py"
--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from flask import Flask
-from flask import url_for
+from flask import url_for  # 
 from markupsafe import escape
-
+from flask import Flask, render_template
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 app = Flask(__name__) #注册
 
 @app.route('/') #添加多个装饰器
 @app.route('/index')
-@app.route('/home')
-def hello():
-    return '<h1>Hello yt!</h1><img src="http://helloflask.com/totoro.gif">'
-
+@app.route('/home')   
+def index():
+    return render_template('index.html', name=name, movies=movies) #把参数传入
 
 @app.route('/user/<name>')
 def user_page(name):
